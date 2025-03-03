7e27112854aa1158349ae5daa79a88806c6d7ab7
MatheusTorresDev97 <matheustorresdev@gmail.com>
2025-03-03 18:17:26 -0300

    criando a conexao com db e add zod

--- restaurante-api/src/middlewares/error-handling.ts
+++ restaurante-api/src/middlewares/error-handling.ts
@@ -1,10 +1,14 @@
 import { ErrorRequestHandler } from "express";
 import { AppError } from "@/utils/AppError";
+import { ZodError } from "zod";
 
 export const errorHandling: ErrorRequestHandler = (error, request, response, next) => {
     if (error instanceof AppError) {
         response.status(error.statusCode).json({ message: error.message });
-    } else {
+    } else if (error instanceof ZodError) {
+        response.status(400).json({ message: "validation error", issues: error.format() })
+    }
+    else {
         response.status(500).json({ message: error.message });
     }
 };
