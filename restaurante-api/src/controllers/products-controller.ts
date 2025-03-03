4f6a7e15450d4c27781c28dc573791bc39831872
MatheusTorresDev97 <matheustorresdev@gmail.com>
2025-03-03 18:25:45 -0300

    validando dados do produto

--- restaurante-api/src/controllers/products-controller.ts
+++ restaurante-api/src/controllers/products-controller.ts
@@ -1,9 +1,25 @@
 import { Request, Response, NextFunction } from "express";
+import z from "zod";
 
 class ProductsController {
     index = async (request: Request, response: Response, next: NextFunction) => {
         try {
             response.json({ message: "OK" });
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    create = async (request: Request, response: Response, next: NextFunction) => {
+        try {
+            const bodySchema = z.object({
+                name: z.string().trim().min(6),
+                price: z.number().gt(0, { message: "value must be greater than 0 " })
+            })
+
+            const { name, price } = bodySchema.parse(request.body)
+
+            response.status(201).json({ name, price })
         } catch (error) {
             next(error);
         }
