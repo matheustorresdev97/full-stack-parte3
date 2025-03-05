5377c0f1d6c39bdf0f2569e9076f12022f96b442
MatheusTorresDev97 <matheustorresdev@gmail.com>
2025-03-05 11:05:17 -0300

    criptografando senha e cadastrando usuario no db

--- rocketlog-api/src/controllers/user-controller.ts
+++ rocketlog-api/src/controllers/user-controller.ts
@@ -1,8 +1,38 @@
 import { Request, Response } from "express";
+import { hash } from "bcrypt"
+import z from "zod";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
 
 class UserController {
     create = async (request: Request, response: Response) => {
-        response.json({ message: "ok" })
+        const bodySchema = z.object({
+            name: z.string().trim().min(2),
+            email: z.string().email(),
+            password: z.string().min(6)
+        })
+
+        const { name, email, password } = bodySchema.parse(request.body)
+
+        const useWithSameEmail = await prisma.user.findFirst({ where: { email } })
+
+        if (useWithSameEmail) {
+            throw new AppError("User with same email already exists")
+        }
+
+        const hashedPassword = await hash(password, 8)
+
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword
+            }
+        })
+
+        const { password: _, ...userWithoutPassword } = user
+
+        response.status(201).json(userWithoutPassword)
     }
 }
 
--- rocketlog-api/src/utils/AppError.ts
+++ rocketlog-api/src/utils/AppError.ts
@@ -2,7 +2,7 @@ class AppError {
     message: string
     statusCode: number
 
-    constructor(message: string, statusCode: number) {
+    constructor(message: string, statusCode: number = 400) {
         this.message = message
         this.statusCode = statusCode
     }
